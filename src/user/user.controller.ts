b42d678bf4542558129a4e7464c18f0f1bb89ef2
Gerardo Grimaldi <grimaldi.gerardo@gmail.com>
2025-04-25 11:33:54 -0300

    erros

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,31 +1,41 @@
 import { Controller, Get, Query, Post, Body, Put, Param, Delete } from '@nestjs/common';
 import { User } from './user.model';
-
+import { HttpStatus } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   private users: User[] = [];
   //`GET /users`: Retrieve a list of all users.
   @Get()
-  findAll(): User[] { // Change return type to any[] to reflect the actual return type
+  findAll(): User[] | HttpStatus{ // Change return type to any[] to reflect the actual return type
+    if (this.users.length === 0) {
+      return  HttpStatus.NOT_FOUND; // Return a default user if the list is empty
+    }
     return this.users;
   }
   // `GET /users/:id`: Retrieve a user by ID.
 
   @Get(':id')
-  findOne(id: number): User | string { // Change return type to User | string
+  findOne(id: number): User | HttpStatus { // Change return type to User | string
     let user = this.users.find(user => user.id === id);
-    return user ? user : `User not found`;
+    return user ? user : HttpStatus.NOT_FOUND; // Return 404 if user not found
   }
+
   // `POST /users`: Add a new user.
   @Post()
   create(req: any): string { 
-    const newUser = req.body; 
+    const newUser = req.body;
+    if (!newUser.name) {  
+      return `User name is required`;
+    }
+    if (!newUser.email) { 
+      return `User email is required`;
+    }
     this.users.push(newUser); 
     return `This action adds a new user with ID: ${newUser.id}`;
   }
-  // `PUT /users/:id`: Update a user by ID.
 
+  // `PUT /users/:id`: Update a user by ID.
   @Put(':id')
   update(id: number, req: any): string {
     
@@ -34,22 +44,19 @@ export class UsersController {
       return `User not found`;
     }
     
-    user = { ...user, ...req.body }; // Assuming req.body contains the updated user data
-    
+    user = { ...user, ...req.body };
     return `This action updates a user with ID: ${id}`;
   }
-  // `DELETE /users/:id`: Delete a user by ID.
 
+  // `DELETE /users/:id`: Delete a user by ID.
   @Delete(':id')
   remove(id: number): string {
     const userIndex = this.users.findIndex(user => user.id === id);
     if (userIndex === -1) {   
       return `User not found`;
     }
-    this.users.splice(userIndex, 1); // Remove the user from the array
-
+    this.users.splice(userIndex, 1);
     return `This action removes a user with ID: ${id}`;
   }
-
-  
+ 
 }
